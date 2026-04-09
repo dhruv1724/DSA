cb4b55b5c9bcf60e8263efcab7a5ff5362e2694c
Dhruv Gandhi <gandhidhruv1724@gmail.com>
2026-04-09 15:44:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (73.84%) - LeetSync

--- Easy/62-unique-paths/unique-paths.cpp
+++ Easy/62-unique-paths/unique-paths.cpp
@@ -13,14 +13,58 @@ public:
         dp[i][j]= way1+way2;;
         return dp[i][j];
     }
+    int findUsingTab(int m,int n){
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        dp[m-1][n-1]=1;
+        //LAST ROW
+        for(int i=0;i<n;i++){
+            dp[m-1][i]=1;
+            //1 hi way hai right jaake
+        }
+        //LAST COLUMN
+        for(int i=0;i<m;i++){
+            dp[i][n-1]=1;
+            //1 hi way hai down jaake
+        }
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                int way1=dp[i+1][j]; //moving down
+                int way2=dp[i][j+1]; //moving right
+                dp[i][j]= way1+way2;;
+            }
+        }
+        return dp[0][0];
+    }
+    //for SO ans depends on down row ka j and current row ka next element
+    int solveUsingSO(int m,int n){
+        vector<int>currentRow(n,-1);
+        vector<int>nextRow(n,-1);
+        
+        //LAST ROW
+        for(int i=0;i<n;i++){
+            currentRow[i]=1;
+            //1 hi way hai right jaake
+        }
+        //LAST COLUMN
+        nextRow[n-1]=1;
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                int way1=currentRow[j]; //moving down
+                int way2=nextRow[j+1]; //moving right
+                nextRow[j]= way1+way2;;
+            }
+            currentRow=nextRow;
+        }
+        return currentRow[0];
+    }
     int uniquePaths(int m, int n) {
         //make relation based on index 
         //do all stuffs
         //find the best way/all possible ways
-        int i=0;
-        int j=0;
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        int ans=find(m,n,i,j,dp);
+        // int i=0;
+        // int j=0;
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        int ans=solveUsingSO(m,n);
         return ans;
     }
 };
