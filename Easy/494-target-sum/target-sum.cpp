2d5d2e51731c0692e7fa849b26610a5b5c4a0600
Dhruv Gandhi <gandhidhruv1724@gmail.com>
2026-04-12 21:56:46 +0530

    Time: 13 ms (53.70%) | Memory: 25.7 MB (26.13%) - LeetSync

--- Easy/494-target-sum/target-sum.cpp
+++ Easy/494-target-sum/target-sum.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    int find(vector<int>&nums,int target,int sum,int i){
+    int find(vector<int>&nums,int i,int target,vector<vector<int>>&dp,int total){
+        if(target + total < 0 || target + total > 2*total) return 0;
+
+        
         if(i==nums.size()){
-            if(target==sum) return 1;
+            if(target==0) return 1;
             return 0;
         }
-        int way1= find(nums,target,sum-nums[i],i+1);
-        int way2=find(nums,target,sum+nums[i],i+1);
-        return way1+way2;
+        if(target+total>=0 && dp[i][target+total]!=-1) return dp[i][target+total];
+        int way1=find(nums,i+1,target-nums[i],dp,total);
+        int way2=find(nums,i+1,target+nums[i],dp,total);
+        return dp[i][target+total]=way1+way2;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum=0;
         int i=0;
-        int ans=find(nums,target,sum,i);
+        int total=0;
+        for(auto i:nums){
+            total+=i;
+        }
+        //note target total can become negative also
+        //so we can get negative index which will give runtime error
+        vector<vector<int>>dp(nums.size(),vector<int>(2*total+1,-1));
+        int ans=find(nums,i,target,dp,total);
         return ans;
     }
 };
